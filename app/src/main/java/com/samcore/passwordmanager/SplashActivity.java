51064cbdcfdad55776ec49f65d960ea7f33c68e9
SPL0019 <118279482+SPL0019@users.noreply.github.com>
2023-12-09 18:51:55 +0530

    Added new: Splash screen and login UI added

--- app/src/main/java/com/samcore/passwordmanager/SplashActivity.java
+++ app/src/main/java/com/samcore/passwordmanager/SplashActivity.java
@@ -3,7 +3,9 @@ package com.samcore.passwordmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
@@ -12,6 +14,8 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> startActivity(new Intent(SplashActivity.this, LoginActivity.class)),3000);
 
     }
 }
